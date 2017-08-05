f83a560160a8de3300ee3fd2f18eeca830275c3b
Scott Colby <scolby33@gmail.com>
2017-08-04 18:21:51 -0700

    Close up blank lines in CLI docopt. Can't have those!

--- src/methodalyze/admin_cli.py
+++ src/methodalyze/admin_cli.py
@@ -5,22 +5,18 @@ Usage:
     methodctl users add <username>
     methodctl users delete <user_id>...
     methodctl users deleteall
-
     methodctl statements list [<statement_id>...]
     methodctl statements add <statement>
     methodctl statements delete <statement_id>...
     methodctl statements deleteall
-
     methodctl methods list [<method_id>...]
     methodctl methods add <method_html>
     methodctl methods delete <method_id>...
     methodctl methods deleteall
-
     methodctl evaluations list [<evaluation_id>...]
     methodctl evaluations add
     methodctl evaluations delete <evaluation_id>...
     methodctl evaluations deleteall
-
     methodctl reset -y
 
 Options:
